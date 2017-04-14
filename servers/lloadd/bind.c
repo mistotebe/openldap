a2f5255070780f69cf9d5b80ea941c2e63b63e16
Ondřej Kuzník <ondra@mistotebe.net>
2017-04-14 09:40:44 +0100

    Report initial bind errors to client

--- servers/lloadd/bind.c
+++ servers/lloadd/bind.c
@@ -123,6 +123,8 @@ request_bind_as_vc( Operation *op )
                *upstream = op->o_upstream;
     BerElement *ber, *request, *copy = NULL;
     BerValue binddn, auth, mech;
+    char *msg = "internal error";
+    int result = LDAP_OTHER;
     ber_int_t version;
     ber_tag_t tag, method;
     ber_len_t len;
@@ -134,6 +136,8 @@ request_bind_as_vc( Operation *op )
 
     tag = ber_scanf( request, "im", &version, &binddn );
     if ( tag == LBER_ERROR || version != LDAP_VERSION3 ) {
+        result = LDAP_PROTOCOL_ERROR;
+        msg = "version not recognised";
         goto fail;
     }
 
@@ -144,6 +148,8 @@ request_bind_as_vc( Operation *op )
 
     tag = ber_skip_element( request, &auth );
     if ( tag == LBER_ERROR ) {
+        result = LDAP_PROTOCOL_ERROR;
+        msg = "malformed bind request";
         goto fail;
     }
 
@@ -205,6 +211,8 @@ request_bind_as_vc( Operation *op )
             ldap_pvt_thread_mutex_unlock( &client->c_mutex );
             break;
         default:
+            result = LDAP_PROTOCOL_ERROR;
+            msg = "malformed bind request";
             goto fail;
     }
 
@@ -225,6 +233,7 @@ fail:
         ber_free( copy, 0 );
     }
     ldap_pvt_thread_mutex_unlock( &upstream->c_write_mutex );
+    operation_send_reject( op, result, msg, 1 );
     ldap_pvt_thread_mutex_lock( &client->c_mutex );
     client_destroy( client );
     return 1;
